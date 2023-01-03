9e2e71165bee66648d51fa27ecde6fa0c76f3b9d
Follow Along <followalong@protonmail.com>
2023-01-03 13:47:21 -0400

    Organize specs / add coverage action

---
+++ use-cases/visit-a-new-feed-via-url.spec.js
@@ -0,0 +1,21 @@
+import { mountApp, buildAddonToRespondWith, rawRSSResponse, describe, test } from './helper.js'
+
+describe('Visit a new feed via URL', () => {
+  // const app = await mountApp()
+  // await app.click('[aria-label="Feeds"]')
+  // expect(app.text()).toContain('Dashboard')
+
+  test('shows the feed title')
+  test('shows the feed image')
+  test('shows the fetched items')
+
+  // event('journeys.create', {
+  //   collection: 'journeys',
+  //   action: 'create',
+  //   data: { color, name }
+  // }, () => { return { app } })
+
+  describe('when the feed already exists', () => {
+    test('shows the existing items')
+  })
+})
---
+++ use-cases/visit-the-changelog.spec.js
@@ -0,0 +1,10 @@
+import { mountApp, buildAddonToRespondWith, rawRSSResponse, describe, test } from './helper.js'
+
+describe('Visit the changelog', () => {
+  // const app = await mountApp()
+  // await app.click('[aria-label="Feeds"]')
+  // expect(app.text()).toContain('Dashboard')
+
+  test('seeds the most recent post')
+  test('fetches the feed')
+})
