f36d22036c95c0e02ee9f7f1d78f1c3dd9cf6aa1
Follow Along <followalong@protonmail.com>
2023-01-06 13:20:38 -0400

    Service worker fun

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './app/component.vue'
 import router from './app/router/index.js'
-import './registerServiceWorker'
 
 createApp(App).use(router).mount('#app')
