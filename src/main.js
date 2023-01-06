2944a249cafd00e55b238f443727f91f44510460
Follow Along <followalong@protonmail.com>
2023-01-06 13:11:38 -0400

    Install PWA

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './app/component.vue'
 import router from './app/router/index.js'
-import './register-service-worker'
+import './registerServiceWorker'
 
 createApp(App).use(router).mount('#app')
