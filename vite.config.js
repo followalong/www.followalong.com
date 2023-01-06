085a158179dae5f41c77630bc9572f1b94426b58
Follow Along <followalong@protonmail.com>
2023-01-06 13:33:19 -0400

    More pwa fun

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
