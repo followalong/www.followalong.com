cfd4fe562411e317f41cfcbcff2482c4ad5a8d33
Follow Along <followalong@protonmail.com>
2023-01-06 13:34:09 -0400

    Cleanup outdated caches

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,9 @@
-import { precacheAndRoute } from 'workbox-precaching'
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
 })
+
+cleanupOutdatedCaches()
 
 precacheAndRoute(self.__WB_MANIFEST)
