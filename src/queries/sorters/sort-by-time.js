b0b9fed3efea64cc20705556dff3577ed53fef23
Follow Along <followalong@protonmail.com>
2023-01-06 21:48:36 -0400

    Fix the sorter

--- src/queries/sorters/sort-by-time.js
+++ src/queries/sorters/sort-by-time.js
@@ -1,5 +1,7 @@
 export default (queries) => {
   return (a, b) => {
+    if (queries.isEntryRead(a) && !queries.isEntryRead(b)) return 1
+    if (!queries.isEntryRead(a) && queries.isEntryRead(b)) return -1
     if (queries.dateForEntry(a).getTime() < queries.dateForEntry(b).getTime()) return 1
     if (queries.dateForEntry(b).getTime() < queries.dateForEntry(a).getTime()) return -1
     return 0
