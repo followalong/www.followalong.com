1afe7ce07b742ad02c02ec3fe5504adeb67ff318
Follow Along <followalong@protonmail.com>
2023-01-12 20:40:59 -0400

    Fix the placement of the new item bar

--- src/app/components/new-bar/component.vue
+++ src/app/components/new-bar/component.vue
@@ -1,7 +1,7 @@
 <template>
   <p
     v-if="newEntriesLength"
-    class="fixed -mt-6 left-0 right-0 p-2 bg-blue-100 primary-color"
+    class="fixed left-0 right-0 p-2 bg-blue-100 primary-color"
   >
     You have {{ newEntriesLength }} new item{{ newEntriesLength === 1 ? '' : 's' }}!
     <a
