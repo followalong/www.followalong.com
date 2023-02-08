d3fa0c814ffa774bcae896480201d640b269551c
Follow Along <followalong@protonmail.com>
2023-02-08 15:23:00 -0400

    Only show the save button if there is anything to save

--- src/app/components/addon-editor/component.vue
+++ src/app/components/addon-editor/component.vue
@@ -89,6 +89,7 @@
               </div>
               <div class="sm:flex sm:flex-row-reverse">
                 <button
+                  v-if="!addon.id || hasFields"
                   type="submit"
                   class="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto"
                 >
@@ -99,7 +100,7 @@
                   class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto"
                   @click="modalOpen = false"
                 >
-                  Cancel
+                  Close
                 </button>
               </div>
             </div>
@@ -130,6 +131,9 @@ export default {
   computed: {
     addonKey () {
       return this.addon.id || this.addon.type
+    },
+    hasFields () {
+      return Object.keys(this.addon.fields).length
     }
   },
   watch: {
