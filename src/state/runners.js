dd429d7128a190b536714228d235205333a702c3
Follow Along <followalong@protonmail.com>
2023-02-08 21:58:00 -0400

    Remove the weird data wrapper

--- src/state/runners.js
+++ src/state/runners.js
@@ -1,19 +1,6 @@
 import EventStore from './event-store'
 import EventStoreEvent from './event-store-event.js'
 
-const EVENT_WITH_POSSIBLE_NESTED_DATA = (type) => {
-  return (store, event) => {
-    if (!event.data || !event.data.data) {
-      return EventStore.RUNNERS[type](store, event)
-    }
-
-    const data = event.data.data ? event.data.data : event.data
-    const newEvent = new EventStoreEvent(event.collection, event.objectId, event.action, data, event.time, event.version)
-
-    return EventStore.RUNNERS[type](store, newEvent)
-  }
-}
-
 const ROLLUP = (store, event) => {
   const identities = [event.data.identity]
   identities.forEach((identity) => {
@@ -53,7 +40,7 @@ const ROLLUP = (store, event) => {
 
 export default {
   'feeds.create': EventStore.RUNNERS.CREATE, // TODO: We can't use nested func because URL is outside of data; OK because URL is immutable for now
-  'feeds.update': EVENT_WITH_POSSIBLE_NESTED_DATA('UPDATE'),
+  'feeds.update': EventStore.RUNNERS.UPDATE,
   'feeds.delete': EventStore.RUNNERS.DELETE,
   'feeds.pause': (store, event) => {
     const collection = store[event.collection]
@@ -78,7 +65,7 @@ export default {
     delete existing.pausedAt
   },
   'entries.create': EventStore.RUNNERS.CREATE, // TODO: We can't use nested func because feedId is outside of data; OK because feedId is immutable for now
-  'entries.update': EVENT_WITH_POSSIBLE_NESTED_DATA('UPDATE'),
+  'entries.update': EventStore.RUNNERS.UPDATE,
   'entries.delete': EventStore.RUNNERS.DELETE,
   'entries.read': (store, event) => {
     const collection = store[event.collection]
