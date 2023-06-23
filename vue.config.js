5c4ca74e87736e26d7b2e26f03807f610a395b71
Follow Along <followalong@protonmail.com>
2023-06-23 10:24:39 -0300

    Add logo

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,23 @@
-let args = {}
-
-if (process.env.NODE_ENV === 'production') {
-  args = {
-    configureWebpack (config) {
-      config.output.filename = '[name].js'
-      config.output.chunkFilename = '[name].js'
+module.exports = {
+  pwa: {
+    name: 'Follow Along',
+    themeColor: '#005B7C',
+    msTileColor: '#005B7C',
+    appleMobileWebAppCapable: 'yes',
+    // appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     },
-    css: {
-      extract: {
-        filename: '[name].css'
-      }
+    workboxOptions: {
+      exclude: ['CNAME'],
+      maximumFileSizeToCacheInBytes: 5000000
     }
+  },
+  css: {
+    sourceMap: process.env.NODE_ENV === 'production'
   }
 }
-
-module.exports = args
