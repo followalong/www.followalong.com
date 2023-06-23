87d37ef596a23942a7dec12499ba5e9bcdb1773b
Follow Along <followalong@protonmail.com>
2023-06-23 10:58:55 -0300

    Add `manifest` namespace

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,21 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      name: 'Follow Along',
-      themeColor: '#005B7C',
-      msTileColor: '#FBF7F0',
-      appleMobileWebAppCapable: 'yes',
-      iconPaths: {
-        favicon32: 'img/icons/favicon-32x32.png',
-        favicon16: 'img/icons/favicon-16x16.png',
-        appleTouchIcon: 'img/icons/apple-touch-icon.png',
-        maskIcon: 'img/icons/safari-pinned-tab.svg',
-        msTileImage: 'img/icons/msapplication-icon-144x144.png'
-      },
-      registerType: 'autoUpdate'
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Follow Along',
+        themeColor: '#005B7C',
+        msTileColor: '#FBF7F0',
+        appleMobileWebAppCapable: 'yes',
+        iconPaths: {
+          favicon: 'img/icons/favicon.ico',
+          favicon32: 'img/icons/favicon-32x32.png',
+          favicon16: 'img/icons/favicon-16x16.png',
+          appleTouchIcon: 'img/icons/apple-touch-icon.png',
+          maskIcon: 'img/icons/safari-pinned-tab.svg',
+          msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        }
+      }
     })
   ],
   test: {
